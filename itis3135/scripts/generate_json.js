f3e00f9f393726fc49bd1e65408093a0cb714102
MarekCasenhiser <100783774+MarekCasenhiser@users.noreply.github.com>
2026-03-18 14:00:56 -0400

    Web design certification

---
+++ itis3135/scripts/generate_json.js
@@ -0,0 +1,85 @@
+
+
+ {
+  "first_name": "Alexander",
+  "preferred_name": "Alex",
+  "middle_initial": "J",
+  "last_name": "Prechtel",
+  "divider": "~",
+  "mascot_adjective": "Advanced",
+  "mascot_animal": "Pegasus",
+  "image": "images/headshot.jpeg",
+  "image_caption": "At the beach on the eastern coast of Florida (2024)",
+  "personal_statement": "...",
+  "personal_background": "Grew up north of Charlotte, and have always had a love of computers.",
+  "professional_background": "This is my first semester as an Instructional Assistant/Teachers Assistant, but before that I was a Peer Tutor for CCI.",
+  "academic_background": "I’m currently a Junior at UNC Charlotte studying computer science with a focus in Cybersecurity. Before that I attended Highschool in Mooresville, North Carolina.",
+  "subject_background": "...",
+  "primary_computer": "The laptop I use for university is a Macbook Pro M2 14 inch. I also use a custom built Windows 11 computer.",
+  "courses": [
+    {
+      "department": "ITIS",
+      "number": "4250",
+      "name": "Computer Forensics",
+      "reason": "Required course for my concentration but the course’s subject also interests me."
+    },
+    {
+      "department": "ITIS",
+      "number": "3246",
+      "name": "IT Infrastructure and Security",
+      "reason": "Another required course for my concentration but this is another class I am interested in."
+    },
+    {
+      "department": "THEA",
+      "number": "1512",
+      "name": "Theatre in the United States",
+      "reason": "I needed to take one more theme course and this one was recommended to me by a friend who took it previously."
+    },
+    {
+      "department": "MATH",
+      "number": "2164",
+      "name": "Matrices & Linear Algebra",
+      "reason": "Required course for the Computer Science degree."
+    },
+    {
+      "department": "BIOL",
+      "number": "1110",
+      "name": "Principles of Biology I",
+      "reason": "I needed to take another science course and this was recommended to me by a friend."
+    },
+    {
+      "department": "BIOL",
+      "number": "1110L",
+      "name": "Principles of Biology I Lab",
+      "reason": "I needed to take a science course with its lab."
+    },
+    {
+      "department": "EXAMPLE",
+      "number": "101",
+      "name": "Example Name",
+      "reason": "Example Reason"
+    }
+  ],
+  "links": [
+    {
+      "name": "GitHub",
+      "href": "..."
+    },
+    {
+      "name": "GitHub Page",
+      "href": "..."
+    },
+    {
+      "name": "freeCodeCamp",
+      "href": "..."
+    },
+    {
+      "name": "Codecademy",
+      "href": "..."
+    },
+    {
+      "name": "LinkedIn",
+      "href": "..."
+    }
+  ]
+}
